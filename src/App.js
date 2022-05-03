e1bfe0a454f53f479113058780deb4b006731692
drewell7 <91854055+drewell7@users.noreply.github.com>
2022-05-03 17:06:53 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import UseFetch from './UseFetch.js';
+import weatherImage from './images/06-s.png';
 
 function App() {
+  const { data, loading, error } = UseFetch("http://dataservice.accuweather.com/forecasts/v1/daily/5day/331216?apikey=3vXnzSU3onkZ9JG1wn46HLA6ElaZtLcc");
+  
+
+  if (loading) return <h1>Loading...</h1>
+
+  if (error) console.log(error);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <img src={weatherImage} alt='' />
+        </div>
+        <h2>{data?.DailyForecasts[0].Temperature.Maximum.Value}</h2>
+        <h2>{data?.DailyForecasts[0].Temperature.Minimum.Value}</h2>
+        <h2>{data?.Headline.Text}</h2>
+        
       </header>
     </div>
   );
---
+++ src/WeatherIcons.js
@@ -0,0 +1,62 @@
+export const WeatherIcons = [
+    {
+        id: 0,
+        name: 'Sunny',
+        image: '/images/01-s.png',
+    },
+    {
+        id: 1,
+        name: 'Sunny',
+        image: '/images/01-s.png',
+    },
+    {
+        id: 2,
+        name: 'Sunny',
+        image: '/images/01-s.png',
+    },
+    {
+        id: 3,
+        name: 'Sunny',
+        image: '/images/01-s.png',
+    },
+    {
+        id: 4,
+        name: 'Sunny',
+        image: '/images/01-s.png',
+    },
+    {
+        id: 5,
+        name: 'Sunny',
+        image: '/images/01-s.png',
+    },
+    {
+        id: 6,
+        name: 'Sunny',
+        image: '/images/01-s.png',
+    },
+    {
+        id: 7,
+        name: 'Sunny',
+        image: '/images/01-s.png',
+    },
+    {
+        id: 8,
+        name: 'Sunny',
+        image: '/images/01-s.png',
+    },
+    {
+        id: 9,
+        name: 'Sunny',
+        image: '/images/01-s.png',
+    },
+    {
+        id: 10,
+        name: 'Sunny',
+        image: '/images/01-s.png',
+    },
+    {
+        id: 11,
+        name: 'Sunny',
+        image: '/images/01-s.png',
+    }
+];
